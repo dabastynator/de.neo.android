db4cd6d08ed068dbc762f9e79defb802a81853e6
Sebastian <sebastian@asus-basti>
2015-08-16 09:58:04 +0200

    Create normal vector array for the gl-torus

--- src/de/neo/android/opengl/figures/GLTorus.java
+++ src/de/neo/android/opengl/figures/GLTorus.java
@@ -9,127 +9,135 @@ import android.graphics.Bitmap;
 
 public class GLTorus extends GLFigure {
 
-	public static int SLICE_MAJOR = 40;
-	public static int SLICE_MINOR = 20;
+	private int mMajorSlices = 40;
+	private int mMinorSlices = 20;
 
-	private FloatBuffer vertexBuffer;
-	private float[] vertex;
+	private FloatBuffer mVertexBuffer;
+	private FloatBuffer mNormalBuffer;
 
 	private FloatBuffer mTextureBuffer;
-	private float[] textureCoordinates;
+	private float[] mTextureCoordinates;
 
-	private ShortBuffer indexBuffer;
-	private short[] indices;
+	private ShortBuffer mIndexBuffer;
+	private short[] mIndices;
 
-	public int paintStyle;
+	public int mPaintStyle;
 
 	public GLTorus(float rmajor, float rminor, int style) {
 		super(style);
-		vertex = new float[(SLICE_MAJOR + 1) * (SLICE_MINOR + 1) * 3];
+		float[] vertex = new float[(mMajorSlices + 1) * (mMinorSlices + 1) * 3];
+		float[] normal = new float[(mMajorSlices + 1) * (mMinorSlices + 1) * 3];
 
-		float stepMajor = (float) ((Math.PI * 2) / SLICE_MAJOR);
-		float stepMinor = (float) ((Math.PI * 2) / SLICE_MINOR);
+		float stepMajor = (float) ((Math.PI * 2) / mMajorSlices);
+		float stepMinor = (float) ((Math.PI * 2) / mMinorSlices);
 
 		// alle Punke für den Ring
-		for (int i = 0; i <= SLICE_MAJOR; i++) {
+		for (int i = 0; i <= mMajorSlices; i++) {
 			float sinMajor = (float) Math.sin(i * stepMajor);
 			float cosMajor = (float) Math.cos(i * stepMajor);
 
-			for (int j = 0; j <= SLICE_MINOR; j++) {
+			for (int j = 0; j <= mMinorSlices; j++) {
 				float sinMinor = (float) Math.sin(j * stepMinor + Math.PI);
 				float cosMinor = (float) Math.cos(j * stepMinor + Math.PI);
 
-				vertex[(i * (SLICE_MINOR + 1) + j) * 3] = sinMajor * rmajor
+				vertex[(i * (mMinorSlices + 1) + j) * 3] = sinMajor * rmajor
 						+ cosMinor * rminor * sinMajor;
-				vertex[(i * (SLICE_MINOR + 1) + j) * 3 + 1] = cosMajor * rmajor
-						+ cosMinor * rminor * cosMajor;
-				vertex[(i * (SLICE_MINOR + 1) + j) * 3 + 2] = sinMinor * rminor;
+				vertex[(i * (mMinorSlices + 1) + j) * 3 + 1] = cosMajor
+						* rmajor + cosMinor * rminor * cosMajor;
+				vertex[(i * (mMinorSlices + 1) + j) * 3 + 2] = sinMinor
+						* rminor;
+
+				normal[(i * (mMinorSlices + 1) + j) * 3] = sinMajor + cosMinor
+						* sinMajor;
+				normal[(i * (mMinorSlices + 1) + j) * 3 + 1] = cosMajor
+						+ cosMinor * cosMajor;
+				normal[(i * (mMinorSlices + 1) + j) * 3 + 2] = sinMinor;
 			}
 		}
 
-		vertexBuffer = allocate(vertex);
+		mVertexBuffer = allocate(vertex);
+		mNormalBuffer = allocate(normal);
 
 		if (style == STYLE_GRID) {
 			makeGridIndices();
-			paintStyle = GL10.GL_LINE_STRIP;
+			mPaintStyle = GL10.GL_LINE_STRIP;
 		}
 
 		if (style == STYLE_PLANE) {
 			makePlaneIndices();
-			paintStyle = GL10.GL_TRIANGLES;
+			mPaintStyle = GL10.GL_TRIANGLES;
 		}
 
 	}
 
 	private void makePlaneIndices() {
-		indices = new short[SLICE_MAJOR * SLICE_MINOR * 6];
+		mIndices = new short[mMajorSlices * mMinorSlices * 6];
 		int j = 0;
 		int i = 0;
 		int counter = 0;
 
 		// Alle Vierecke werden als zwei Dreiecke gezeichnet
 
-		for (i = 0; i < SLICE_MAJOR; i++) {
-			for (j = 0; j < SLICE_MINOR; j++) {
+		for (i = 0; i < mMajorSlices; i++) {
+			for (j = 0; j < mMinorSlices; j++) {
 				// erstes Dreieck
-				indices[counter++] = (short) (i * (SLICE_MINOR + 1) + j);
-				indices[counter++] = (short) (i * (SLICE_MINOR + 1) + j + 1);
-				indices[counter++] = (short) ((i + 1) * (SLICE_MINOR + 1) + j);
+				mIndices[counter++] = (short) (i * (mMinorSlices + 1) + j);
+				mIndices[counter++] = (short) (i * (mMinorSlices + 1) + j + 1);
+				mIndices[counter++] = (short) ((i + 1) * (mMinorSlices + 1) + j);
 
 				// zweites Dreieck
-				indices[counter++] = (short) (i * (SLICE_MINOR + 1) + j + 1);
-				indices[counter++] = (short) ((i + 1) * (SLICE_MINOR + 1) + j + 1);
-				indices[counter++] = (short) ((i + 1) * (SLICE_MINOR + 1) + j);
+				mIndices[counter++] = (short) (i * (mMinorSlices + 1) + j + 1);
+				mIndices[counter++] = (short) ((i + 1) * (mMinorSlices + 1) + j + 1);
+				mIndices[counter++] = (short) ((i + 1) * (mMinorSlices + 1) + j);
 			}
 		}
 
-		indexBuffer = allocate(indices);
+		mIndexBuffer = allocate(mIndices);
 
 		// Textur mit Vertex verbinden
-		textureCoordinates = new float[(SLICE_MAJOR + 1) * (SLICE_MINOR + 1)
+		mTextureCoordinates = new float[(mMajorSlices + 1) * (mMinorSlices + 1)
 				* 2];
-		for (i = 0; i <= SLICE_MAJOR; i++)
-			for (j = 0; j <= SLICE_MINOR; j++) {
-				textureCoordinates[(i * (SLICE_MINOR + 1) + j) * 2] = ((float) i)
-						/ SLICE_MAJOR;
-				textureCoordinates[(i * (SLICE_MINOR + 1) + j) * 2 + 1] = ((float) j)
-						/ SLICE_MINOR;
+		for (i = 0; i <= mMajorSlices; i++)
+			for (j = 0; j <= mMinorSlices; j++) {
+				mTextureCoordinates[(i * (mMinorSlices + 1) + j) * 2] = ((float) i)
+						/ mMajorSlices;
+				mTextureCoordinates[(i * (mMinorSlices + 1) + j) * 2 + 1] = ((float) j)
+						/ mMinorSlices;
 			}
 
-		mTextureBuffer = allocate(textureCoordinates);
+		mTextureBuffer = allocate(mTextureCoordinates);
 
 	}
 
 	private void makeGridIndices() {
-		indices = new short[(SLICE_MAJOR + 1) * (SLICE_MINOR + 1) * 2];
+		mIndices = new short[(mMajorSlices + 1) * (mMinorSlices + 1) * 2];
 		int j = 0;
 		// alle kleinen Ringe werden gezeichnet plus einem Großem
-		for (int i = 0; i < (SLICE_MAJOR + 1) * (SLICE_MINOR + 1); i++) 
-			indices[j++] = (short) i;
-		
-		// alle restlichen großen Ringe
-		for (int i = 1; i < SLICE_MINOR; i++)
-			for (int k = 0; k <= SLICE_MAJOR; k++)
-				indices[j++] = (short) (k * (SLICE_MINOR + 1) + i);
+		for (int i = 0; i < (mMajorSlices + 1) * (mMinorSlices + 1); i++)
+			mIndices[j++] = (short) i;
 
-		indexBuffer = allocate(indices);
+		// alle restlichen großen Ringe
+		for (int i = 1; i < mMinorSlices; i++)
+			for (int k = 0; k <= mMajorSlices; k++)
+				mIndices[j++] = (short) (k * (mMinorSlices + 1) + i);
+
+		mIndexBuffer = allocate(mIndices);
 	}
-	
 
 	public void setTexture(Bitmap b, float scale) {
 		super.setTexture(b);
 		// Textur mit Vertex verbinden
-		textureCoordinates = new float[(SLICE_MAJOR + 1) * (SLICE_MINOR + 1)
+		mTextureCoordinates = new float[(mMajorSlices + 1) * (mMinorSlices + 1)
 				* 2];
-		for (int i = 0; i <= SLICE_MAJOR; i++)
-			for (int j = 0; j <= SLICE_MINOR; j++) {
-				textureCoordinates[(i * (SLICE_MINOR + 1) + j) * 2] = (scale * i)
-						/ SLICE_MAJOR;
-				textureCoordinates[(i * (SLICE_MINOR + 1) + j) * 2 + 1] = ((float) j)
-						/ SLICE_MINOR;
+		for (int i = 0; i <= mMajorSlices; i++)
+			for (int j = 0; j <= mMinorSlices; j++) {
+				mTextureCoordinates[(i * (mMinorSlices + 1) + j) * 2] = (scale * i)
+						/ mMajorSlices;
+				mTextureCoordinates[(i * (mMinorSlices + 1) + j) * 2 + 1] = ((float) j)
+						/ mMinorSlices;
 			}
 
-		mTextureBuffer = allocate(textureCoordinates);
+		mTextureBuffer = allocate(mTextureCoordinates);
 	}
 
 	@Override
@@ -139,18 +147,21 @@ public class GLTorus extends GLFigure {
 		gl.glCullFace(GL10.GL_BACK);
 
 		gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
-		gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
-
+		gl.glVertexPointer(3, GL10.GL_FLOAT, 0, mVertexBuffer);
+		gl.glEnableClientState(GL10.GL_NORMAL_ARRAY);
+		gl.glNormalPointer(GL10.GL_FLOAT, 0, mNormalBuffer);
+		
 		gl.glDisableClientState(GL10.GL_COLOR_ARRAY);
 
 		if (mTexture != null && mTextureBuffer != null)
 			gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, mTextureBuffer);
 
 		// Punke zeichnen
-		gl.glDrawElements(paintStyle, indices.length, GL10.GL_UNSIGNED_SHORT,
-				indexBuffer);
+		gl.glDrawElements(mPaintStyle, mIndices.length, GL10.GL_UNSIGNED_SHORT,
+				mIndexBuffer);
 
 		gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+		gl.glDisableClientState(GL10.GL_NORMAL_ARRAY);
 		gl.glDisable(GL10.GL_CULL_FACE);
 	}
 
